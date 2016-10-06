d8aef32f9ccd853c891b7bc67c40f81c28764a0a
Fergus Waugh <fjswaugh@gmail.com>
2016-10-06 20:05:15 +0100

    Reorganised functions that read from file into a single .m file

--- read_data.m
+++ read_data.m
@@ -53,3 +53,100 @@ function data = read_data(em_filename, ps_filename, screen_res)
     
     data = Data(raw_ps_data, em_data, meta);
 end
+
+function [data] = get_next_em_data(file)
+    % Use a cell array to store data because it grows much faster than a
+    % matrix. We will then convert the array to a matrix after it's full.
+    cell_array = {};
+
+    % State variables, all three must be true to collect data.
+    eye_open = true;
+    fixed = false;
+    synctime_found = false;
+
+    index = 0;  % An index so that we know where to add data to cell array.
+    line = fgetl(file);
+    while not(str_contains(line, 'END'))
+        if str_contains(line, 'SYNCTIME')
+            synctime_found = true;
+        elseif str_contains(line, 'SFIX')
+            fixed = true;
+        elseif str_contains(line, 'EFIX')
+            fixed = false;
+        elseif str_contains(line, 'SBLINK')
+            eye_open = false;
+        elseif str_contains(line, 'EBLINK')
+            eye_open = true;
+        % All legitimate data lines end in ... (so test this to make sure)
+        elseif str_contains(line, '...')
+            if synctime_found && fixed && eye_open
+                index = index + 1;
+                cell_array(index) = {cell2mat(textscan(line, '%f', 6))};
+            end
+        end
+
+        line = fgetl(file);
+    end
+
+    % Finally convert the cell array to a matrix.
+    data = cell2mat(cell_array)';
+end
+
+function [data] = get_ps_data(file)
+    collect_data = false;
+    trial_num = 1;
+    cell_array = {};
+    
+    line = fgetl(file);
+    while ischar(line)
+        if collect_data
+            cell_array(trial_num) = {cell2mat(textscan(line, '%f', 6))};
+            trial_num = trial_num + 1;
+        elseif str_contains(line, 'Reversals')
+            collect_data = true;
+            fgetl(file);
+        end
+        
+        line = fgetl(file);
+    end
+    
+    data = cell2mat(cell_array)';
+end
+
+function [meta] = get_meta(file)
+    meta = struct('flank', 'Iso', 'color', 'Black', 'luminance', 0);
+    
+    line = fgetl(file);
+    while ischar(line)
+        if str_contains(line, 'features')
+            txt = textscan(line, '%s', 5);
+            txt = txt{1}{5};
+            if strcmp(txt, 'Cambridge')
+                meta.flank = 'Similar';
+            elseif strcmp(txt, 'a')
+                meta.flank = 'Box';
+            elseif strcmp(txt, 'bars')
+                meta.flank = 'Bars';
+            end
+        elseif str_contains(line, 'luminance')
+            txt = textscan(line, '%s', 5);
+            txt = txt{1}{5};
+            txt = txt(1:length(txt)-1);
+            meta.luminance = str2num(txt);
+        elseif str_contains(line, 'crowding colour')
+            txt = textscan(line, '%s', 6);
+            color = [str2num(txt{1}{4}),...
+                     str2num(txt{1}{5}),...
+                     str2num(txt{1}{6})];
+            if color(1) == -254
+                meta.color = 'Red';
+            elseif color(2) == -254;
+                meta.color = 'Green';
+            elseif color(3) == -254;
+                meta.color = 'Blue';
+            end
+        end
+        
+        line = fgetl(file);
+    end
+end
