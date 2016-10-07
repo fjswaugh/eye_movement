8142ab4d3abeea67d52d4952f203e7537b9d96e8
Fergus Waugh <fjswaugh@gmail.com>
2016-10-07 13:41:24 +0100

    Added function to generate table

---
+++ generate_table.m
@@ -0,0 +1,28 @@
+function generate_table(data)
+%generate_table Prints a table of the data in a figure
+    
+    f = figure;
+    t = uitable(f);
+    
+    td = cell(data.size(), 8);
+    
+    td(:, 1) = num2cell(data.trial_num);
+    td(:, 2) = num2cell(data.logmar);
+    td(:, 3) = cellstr(data.image_char);
+    td(:, 4) = data.type_str();
+    for i = 1:data.size()
+        td{i, 5} = std(data.em_data{i}.xdeg);
+        td{i, 6} = std(data.em_data{i}.ydeg);
+        td{i, 7} = pearson(data.em_data{i}.xdeg, data.em_data{i}.ydeg);
+    end
+    td(:, 8) = num2cell(data.bcea);
+    
+    t.Data = td;
+    t.ColumnName = {'Trial',    'LogMAR',  'Image char', 'Type',...
+                    'σ(xdeg)',  'σ(ydeg)', 'Pearson',    'BCEA'};
+    t.BackgroundColor = ones(data.size(), 3);
+    t.BackgroundColor(:, 1) = (-204/255) * data.desirable + 1;
+    t.BackgroundColor(:, 2) = (-119/255) * data.desirable + 1;
+    t.ColumnWidth = {80, 120, 90, 150, 120, 120, 120, 120};
+    t.Position = [0, 0, 960, 600];
+end
--- gui.m
+++ gui.m
@@ -195,29 +195,7 @@ function button_read_data_Callback(hObject, eventdata, handles)
 end
 
 function button_table_Callback(hObject, eventdata, handles)
-    f = figure;
-    d = handles.all_data;
-    t = uitable(f);
-    td = cell(d.size(), 8);
-    td(:, 1) = num2cell(d.trial_num);
-    td(:, 2) = num2cell(d.logmar);
-    td(:, 3) = cellstr(d.image_char);
-    td(:, 4) = d.type_str();
-    for i = 1:d.size()
-        td{i, 5} = std(d.em_data{i}.xdeg);
-        td{i, 6} = std(d.em_data{i}.ydeg);
-        td{i, 7} = pearson(d.em_data{i}.xdeg, d.em_data{i}.ydeg);
-    end
-    td(:, 8) = num2cell(d.bcea);
-    
-    t.Data = td;
-    t.ColumnName = {'Trial',    'LogMAR',  'Image char', 'Type',...
-                    'σ(xdeg)',  'σ(ydeg)', 'Pearson',    'BCEA'};
-    t.BackgroundColor = ones(d.size(), 3);
-    t.BackgroundColor(:, 1) = (-204/255) * d.desirable + 1;
-    t.BackgroundColor(:, 2) = (-119/255) * d.desirable + 1;
-    t.ColumnWidth = {80, 120, 90, 150, 120, 120, 120, 120};
-    t.Position = [0, 0, 960, 600];
+    generate_table(handles.all_data);
 end
 
 function start_limit_edit_Callback(hObject, eventdata, handles)
