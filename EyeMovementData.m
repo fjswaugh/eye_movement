8be3437a5c3be8e84247f6c3306a421abc19ee96
Fergus Waugh <fjswaugh@gmail.com>
2016-10-08 21:34:00 +0100

    Changed class implementation so user can now modify data points if they wish (even with limits set)

--- EyeMovementData.m
+++ EyeMovementData.m
@@ -7,14 +7,9 @@
 % data1.copy() (otherwise the class cannot be copied, only new references
 % to the same data created).
 classdef EyeMovementData < matlab.mixin.Copyable
-    % Let properties be set only from within the constructor (before limits
-    % have been applied)
-    % Otherwise anyone editing these variables with the limits applied will
-    % end up overwriting all data not within the limits. If editing is
-    % required a different design approach for the class would be required,
-    % perhaps using copies of each data set that were created when limits
-    % are set and merged back into the master set when limits are removed.
-    properties (SetAccess = immutable)
+    % These are the public facing properties. They represent the data
+    % within the limits set.
+    properties
         trial_num;
         time;  % Time data, running from 1 -> ~1000
         xpix;  % x pixel data
@@ -22,15 +17,26 @@ classdef EyeMovementData < matlab.mixin.Copyable
         xdeg;
         ydeg;
         pupil_area;
+
     end
     
-    % These are simply internal to the class's workings
+    % These are properties internal to the class's workings
     properties (Access = private)
         % If you wish to only look at part of the data when calculating
         % BCEA (for example), then limits can be set on the subsection
         % of milliseconds to look at.
         s_;  % Start limit
         e_;  % Finish limit
+        
+        % Mastrer data. These are kept private since they are not directly
+        % accessed by users of the class. Instead a copy of them is kept
+        % public that represents the data within whatever limits are set.
+        time_;
+        xpix_;
+        ypix_;
+        xdeg_;
+        ydeg_;
+        pupil_area_;
         
         size_;  % Original size
     end
@@ -49,44 +55,35 @@ classdef EyeMovementData < matlab.mixin.Copyable
             
             obj.trial_num = trial_num;
             
-            obj.time = (1:obj.size_)';
+            % Set the master copies of the data
             
-            obj.xpix = raw_data(:,2) - offset(1);
-            obj.ypix = raw_data(:,3) - offset(2);
+            obj.time_ = (1:obj.size_)';
+            obj.xpix_ = raw_data(:,2) - offset(1);
+            obj.ypix_ = raw_data(:,3) - offset(2);
             
             xres = raw_data(:,5);
             yres = raw_data(:,6);
             % x and y data in degrees relative to centre
-            obj.xdeg = (obj.xpix - screen_res(1)/2) ./ xres;
-            obj.ydeg = (obj.ypix - screen_res(2)/2) ./ yres;
+            obj.xdeg_ = (obj.xpix_ - screen_res(1)/2) ./ xres;
+            obj.ydeg_ = (obj.ypix_ - screen_res(2)/2) ./ yres;
             
-            obj.pupil_area = raw_data(:,4);
+            obj.pupil_area_ = raw_data(:,4);
+            
+            % Set the public copies of the data to be the same as the
+            % master copies (at least until the limits are changed)
+            
+            obj.time       = obj.time_;
+            obj.xpix       = obj.xpix_;
+            obj.ypix       = obj.ypix_;
+            obj.xdeg       = obj.xdeg_;
+            obj.ydeg       = obj.ydeg_;
+            obj.pupil_area = obj.pupil_area_;
         end
 
         % size() returns the original size of the object, not the size
         % between the limits
         function s = size(obj)
             s = obj.size_;
-        end
-        
-        % Get functions, returning the relevant data only (within the limits)
-        function t  = get.time(obj)
-            t  = obj.time(obj.s_:obj.e_);
-        end
-        function xp = get.xpix(obj)
-            xp = obj.xpix(obj.s_:obj.e_);
-        end
-        function yp = get.ypix(obj)
-            yp = obj.ypix(obj.s_:obj.e_);
-        end
-        function xd = get.xdeg(obj)
-            xd = obj.xdeg(obj.s_:obj.e_);
-        end
-        function yd = get.ydeg(obj)
-            yd = obj.ydeg(obj.s_:obj.e_);
-        end
-        function pa = get.pupil_area(obj)
-            pa = obj.pupil_area(obj.s_:obj.e_);
         end
         
         function l = limits(obj)
@@ -98,30 +95,100 @@ classdef EyeMovementData < matlab.mixin.Copyable
         end
 
         function remove_limits(obj)
+            % Merge the public data back with the master data, preserving
+            % any changes made
+            obj.time_(obj.s_:obj.e_)       = obj.time;
+            obj.xpix_(obj.s_:obj.e_)       = obj.xpix;
+            obj.ypix_(obj.s_:obj.e_)       = obj.ypix;
+            obj.xdeg_(obj.s_:obj.e_)       = obj.xdeg;
+            obj.ydeg_(obj.s_:obj.e_)       = obj.ydeg;
+            obj.pupil_area_(obj.s_:obj.e_) = obj.pupil_area;
+            
             obj.s_ = 1;
             obj.e_ = obj.size();
+            
+            % Reset the public data to be the same as the master data
+            obj.time       = obj.time_;
+            obj.xpix       = obj.xpix_;
+            obj.ypix       = obj.ypix_;
+            obj.xdeg       = obj.xdeg_;
+            obj.ydeg       = obj.ydeg_;
+            obj.pupil_area = obj.pupil_area_;
         end
         
         function set_limits(obj, start, finish)
             if finish < start
                 error('End value cannot be smaller than start value');
-            end
-            if start < 1 || finish < 1
+            elseif start < 1 || finish < 1
                 error('Values cannot be smaller than 1');
-            end
-            if start > obj.size()
+            elseif start > obj.size()
                 error('Start limit outside data');
             end
+            
             if finish > obj.size()
-                warning(['End limit is larger than data size, and so has '...
-                         'been set to its maximum']);
+                warning(['End limit is larger than data size, and so '...
+                         'has been set to its maximum']);
                 finish = obj.size();
             end
+            
+            % First remove limits to save any changes made to the
+            % public data
+            obj.remove_limits();
        
             obj.s_ = start;
             obj.e_ = finish;
+            
+            obj.time       = obj.time_(obj.s_:obj.e_);
+            obj.xpix       = obj.xpix_(obj.s_:obj.e_);
+            obj.ypix       = obj.ypix_(obj.s_:obj.e_);
+            obj.xdeg       = obj.xdeg_(obj.s_:obj.e_);
+            obj.ydeg       = obj.ydeg_(obj.s_:obj.e_);
+            obj.pupil_area = obj.pupil_area_(obj.s_:obj.e_);
         end
-
+        
+        % Set methods allow restrictions to be placed when setting the
+        % public data, to preserve the integrity of the class
+        function obj = set.time(obj, t)
+            if size(t, 1) == obj.e_ - obj.s_ + 1
+                obj.time = t;
+            else
+                error('Length mismatch');
+            end
+        end
+        function obj = set.xpix(obj, xp)
+            if size(xp, 1) == obj.e_ - obj.s_ + 1
+                obj.xpix = xp;
+            else
+                error('Length mismatch');
+            end
+        end
+        function obj = set.ypix(obj, yp)
+            if size(yp, 1) == obj.e_ - obj.s_ + 1
+                obj.ypix = yp;
+            else
+                error('Length mismatch');
+            end
+        end
+        function obj = set.xdeg(obj, xd)
+            if size(xd, 1) == obj.e_ - obj.s_ + 1
+                obj.xdeg = xd;
+            else
+                error('Length mismatch');
+            end
+        end
+        function obj = set.ydeg(obj, yd)
+            if size(yd, 1) == obj.e_ - obj.s_ + 1
+                obj.ydeg = yd;
+            else
+                error('Length mismatch');
+            end
+        end
+        function obj = set.pupil_area(obj, pa)
+            if size(pa, 1) == obj.e_ - obj.s_ + 1
+                obj.pupil_area = pa;
+            else
+                error('Length mismatch');
+            end
+        end
     end
 end
-
